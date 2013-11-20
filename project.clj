4132eeb1f7a6739bd372a22bc47d3d5314af8383
Björn Roberg <bjorn.roberg@gmail.com>
2013-11-20 22:24:40 +0100

    cleanup in project

--- project.clj
+++ project.clj
@@ -3,11 +3,5 @@
   :url "http://stevelosh.com/blog/2012/07/caves-of-clojure-01/"
   :license {:name "MIT/X11"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojure-lanterna "0.9.2"]
-                ; [com.googlecode.lanterna/lanterna "2.0.1-SNAPSHOT"]
-                 ]
-
-  ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
-  :main ^{:skip-aot true} caves.core
-
-  )
+                 [clojure-lanterna "0.9.2"]]
+  :main ^{:skip-aot true} caves.core)
